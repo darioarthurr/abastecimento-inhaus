d8e23157b0c4f37c4fa292f32d6d5bdedc6b7fe7
darioarthurr <darioarthurr@gmail.com>
2026-08-26 15:23:56 -0300

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='abastecimento-v2';
+const CACHE='abastecimento-v2-1';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
 
 self.addEventListener('install',e=>{
